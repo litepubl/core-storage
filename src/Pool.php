ea40762314b6c51b734ecf66e1667b788bc11a79
litepubl <yarrowsoft@gmail.com>
2017-05-28 23:06:27 +0300

    rc7.08 pool updated

--- src/Pool.php
+++ src/Pool.php
@@ -6,29 +6,31 @@ use litepubl\core\Storage\Locker;
 
 class Pool implements PoolInterface, StorableInterface
 {
+    protected $baseName = 'storage';
     protected $data;
-    protected $instances;
+    protected $items;
     protected $storage;
+    protected $modified;
     private $locker;
-    private $modified;
 
     public function __construct(StorageInterface $storage, LockerInterface $locker)
     {
         $this->storage = $storage;
         $this->locker = $locker;
-        $this->instances = [];
+        $this->items = [];
         $this->data = [];
+        $this->modified = false;
         $storage->load($this);
     }
 
     public function getBaseName(): string
     {
-        return 'storage';
+        return $this->baseName;
     }
 
     public function getData(): array
     {
-        foreach ($this->instances as $name => $storable) {
+        foreach ($this->items as $name => $storable) {
                $this->data[$name] = $storable->getData();
         }
 
@@ -48,24 +50,22 @@ class Pool implements PoolInterface, StorableInterface
     public function has(StorableInterface $storable): bool
     {
         $base = $storable->getBaseName();
-        return isset($this->data[$base]) || isset($this->instances[$base]);
+        return isset($this->data[$base]) || isset($this->items[$base]);
     }
 
     public function save(StorableInterface $storable): bool
     {
-        $this->modified = true;
-        $base = $storable->getBaseName();
-        $this->instances[$base] = $storable;
+        $this->items[$storable->getBaseName()] = $storable;
         return true;
     }
 
     public function load(StorableInterface $storable): bool
     {
-        $base = $storable->getbasename();
+        $base = $storable->getBaseName();
         if (isset($this->data[$base])) {
             $storable->setData($this->data[$base]);
-        } elseif (isset($this->instances[$base]) && ($storable != $this->instances[$base])) {
-            $storable->setData($this->instances[$base]->getData());
+        } elseif (isset($this->items[$base]) && ($storable != $this->items[$base])) {
+            $storable->setData($this->items[$base]->getData());
         } else {
                 return false;
         }
@@ -75,19 +75,20 @@ class Pool implements PoolInterface, StorableInterface
 
     public function remove(StorableInterface $storable): bool
     {
-        $base = $storable->getbasename();
+        $base = $storable->getBaseName();
         if (isset($this->data[$base])) {
             unset($this->data[$base]);
             $result = true;
         }
 
-        if (isset($this->instances[$base])) {
-            unset($this->instances[$base]);
+        if (isset($this->items[$base])) {
+            unset($this->items[$base]);
             $result = true;
         }
 
+
         if ($result) {
-            $this->modified = true;
+                $this->modified = true;
         }
 
             return $result;
@@ -95,22 +96,23 @@ class Pool implements PoolInterface, StorableInterface
 
     public function commit(): bool
     {
-        if (!$this->modified) {
-            return false;
+        if (count($this->items) || $this->modified) {
+            if ($this->locker->lock()) {
+                try {
+                      $this->storage->save($this);
+
+                     $this->items = [];
+                    $this->modified = false;
+                } finally {
+                    $this->locker->unlock();
+                }
+                return true;
+            } else {
+                $this->error('Storage locked, data not saved');
+            }
         }
 
-        if ($this->locker->lock()) {
-            try {
-                      $this->storage->save($this);
-                      $this->modified = false;
-            } finally {
-                $this->locker->unlock();
-            }
-                  return true;
-        } else {
-            $this->error('Storage locked, data not saved');
             return false;
-        }
     }
 
     public function getInstalled(): bool
